357eee4c8ee0603917449f91938173948fb223e7
annika <a.schuster@hoch2.de>
2016-02-01 16:00:40 +0100

    Add Actions

---
+++ Classes/Controller/AddressController.php
@@ -0,0 +1,37 @@
+<?php
+namespace Hochzwei\H2dmailsub\Controller;
+
+/**
+ * AddressController
+ *
+ */
+class AddressController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+    /**
+     * AddressRepository
+     *
+     * @var TYPO3\TtAddress\Domain\Repository
+     * @inject
+     */
+    protected $addressRepository = null;
+
+    /**
+     * subscribe
+     *
+     * @return void
+     */
+    public function subscribeAction()
+    {
+
+    }
+
+    /**
+     * unsubscribe
+     *
+     * @return void
+     */
+    public function unsubscribeAction()
+    {
+
+    }
+}
