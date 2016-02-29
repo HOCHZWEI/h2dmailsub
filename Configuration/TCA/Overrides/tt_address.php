25e2bd5c67297475415d689d5bb341cae3484061
annika <a.schuster@hoch2.de>
2016-02-29 17:29:29 +0100

    added fields for subscribe/ add radio buttons for gender in tca closes #2

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -2,15 +2,18 @@
 defined('TYPO3_MODE') or die();
 
 $fields = [
-	'localgender' => [
-		'exclude' => 1,
-		'label' => 'LLL:EXT:h2dmailsub/Resources/Private/Language/locallang_db.xlf:tx_h2dmailsub_domain_model_address.localgender',
-		'config' => [
-			'type' => 'input',
-			'size' => 30,
-			'eval' => 'trim'
-		],
-	],
+    'localgender' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:h2dmailsub/Resources/Private/Language/locallang_db.xlf:tx_h2dmailsub_domain_model_address.localgender',
+        'config' => [
+            'type' => 'radio',
+            'items' => array(
+                array('LLL:EXT:h2dmailsub/Resources/Private/Language/locallang_db.xlf:tx_h2dmailsub_domain_model_address.localgender.female', 'f'),
+                array('LLL:EXT:h2dmailsub/Resources/Private/Language/locallang_db.xlf:tx_h2dmailsub_domain_model_address.localgender.male', 'm'),
+                array('LLL:EXT:h2dmailsub/Resources/Private/Language/locallang_db.xlf:tx_h2dmailsub_domain_model_address.localgender.none', 'n')
+            )
+        ],
+    ],
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_address', $fields);
